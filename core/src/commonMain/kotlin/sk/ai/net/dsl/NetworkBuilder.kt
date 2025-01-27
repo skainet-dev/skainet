10208b3f9bde177cf3c0b0f1fe736bfc0623bd99
Michal Harakal <michal.harakal@googlemail.com>
2025-01-27 23:08:51 +0100

    Remove broken android test from the temple.

---
+++ core/src/commonMain/kotlin/sk/ai/net/dsl/NetworkBuilder.kt
@@ -0,0 +1,109 @@
+package sk.ai.net.dsl
+
+import sk.ai.net.nn.activations.ActivationsWrapperModule
+import sk.ai.net.Shape
+import sk.ai.net.Tensor
+import sk.ai.net.nn.Input
+import sk.ai.net.nn.Linear
+import sk.ai.net.nn.Module
+import sk.ai.net.nn.topology.MLP
+
+
+// DSL Marker to restrict the DSL to its intended scope
+@DslMarker
+annotation class NetworkDsl
+
+@NetworkDsl
+fun network(content: NeuralNetworkDsl.() -> Unit) = NeuralNetworkDslImpl()
+    .apply(content)
+    .create()
+
+@NetworkDsl
+interface NetworkDslItem
+
+@NetworkDsl
+interface NeuralNetworkDsl : NetworkDslItem {
+    fun input(inputSize: Int, id: String = "")
+
+    fun dense(outputDimension: Int, id: String = "", content: DENSE.() -> Unit = {})
+}
+
+@NetworkDsl
+interface DENSE : NetworkDslItem {
+    var activation: (Tensor) -> Tensor
+    fun weights(initBlock: (Shape) -> Tensor)
+    fun bias(initBlock: (Shape) -> Tensor)
+}
+
+private fun getDefaultName(id: String, s: String, size: Int): String {
+    if (id.isNotEmpty()) return id
+    return "$s-$size"
+}
+
+
+class DenseImpl(
+    private val inputDimension: Int, private val outputDimension: Int, private val id: String
+) : DENSE {
+
+    private var weightsValue: Tensor? = null
+    private var _activation: (Tensor) -> Tensor = { tensor -> tensor }
+
+    fun create(): List<Module> {
+        return listOf(
+            Linear(inputDimension, outputDimension, id, weightsValue!!, weightsValue!!),
+            ActivationsWrapperModule(activation, "activation")
+        )
+    }
+
+    override var activation: (Tensor) -> Tensor
+        get() = _activation
+        set(value) {
+            _activation = value
+        }
+
+    override fun weights(initBlock: (Shape) -> Tensor) {
+        weightsValue = initBlock(Shape(outputDimension, inputDimension))
+    }
+
+    override fun bias(initBlock: (Shape) -> Tensor) {
+        weightsValue = initBlock(Shape(outputDimension))
+    }
+}
+
+private class NeuralNetworkDslImpl : NeuralNetworkDsl {
+
+    val modules = mutableListOf<Module>()
+    var lastDimension = 0
+
+    fun create() = NetworkBuilder().add(*modules.toTypedArray()).build()
+    override fun input(inputSize: Int, id: String) {
+        lastDimension = inputSize
+        modules.add(Input(Shape(inputSize), name = getDefaultName(id, "Input", modules.size)))
+    }
+
+    override fun dense(outputDimension: Int, id: String, content: DENSE.() -> Unit) {
+        val inputDimension = lastDimension
+        lastDimension = outputDimension
+        val impl = DenseImpl(
+            inputDimension = inputDimension,
+            outputDimension = outputDimension,
+            id = getDefaultName(id, "linear", modules.size)
+        )
+        impl.content()
+        // dense layer consinst from linear module and activation function module (2 modules)
+        modules += impl.create()
+    }
+}
+
+
+@NetworkDsl
+class NetworkBuilder {
+    private val modules = mutableListOf<Module>()
+
+    fun add(vararg modules: Module): NetworkBuilder {
+        this.modules += modules.toList()
+        return this
+    }
+
+    fun build(): Module = MLP(*modules.toTypedArray())
+}
