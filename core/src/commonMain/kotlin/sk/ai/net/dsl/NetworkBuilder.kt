1a586e86c96dbe5729ff90b51da54903d70b6e06
Michal Harakal <michal.harakal@googlemail.com>
2025-06-08 23:42:28 +0200

    Add support for stages with DSL

--- core/src/commonMain/kotlin/sk/ai/net/dsl/NetworkBuilder.kt
+++ core/src/commonMain/kotlin/sk/ai/net/dsl/NetworkBuilder.kt
@@ -35,13 +35,20 @@ interface NeuralNetworkDsl : NetworkDslItem {
     fun maxPool2d(id: String = "", content: MAXPOOL2D.() -> Unit = {})
 
     fun dense(outputDimension: Int, id: String = "", content: DENSE.() -> Unit = {})
+    
+    fun dense(id: String = "", content: DENSE.() -> Unit = {})
 
     fun activation(id: String = "", activation: (Tensor) -> Tensor)
+    
+    fun sequential(content: NeuralNetworkDsl.() -> Unit)
+    
+    fun stage(id: String, content: NeuralNetworkDsl.() -> Unit)
 }
 
 @NetworkDsl
 interface DENSE : NetworkDslItem {
     var activation: (Tensor) -> Tensor
+    var units: Int
     fun weights(initBlock: (Shape) -> Tensor)
     fun bias(initBlock: (Shape) -> Tensor)
 }
@@ -112,18 +119,21 @@ class FlattenImpl(
 }
 
 class DenseImpl(
-    private val inputDimension: Int, private val outputDimension: Int, private val id: String
+    private val inputDimension: Int, private var _outputDimension: Int, private val id: String
 ) : DENSE {
 
     private var weightsValue: Tensor? = null
     private var biasValue: Tensor? = null
     private var _activation: (Tensor) -> Tensor = { tensor -> tensor }
 
-    fun create(): List<Module> {
+    // Expose the output dimension
+    val outputDimension: Int
+        get() = _outputDimension
 
+    fun create(): List<Module> {
         val linear = createLinear(
             inFeatures = inputDimension,
-            outFeatures = outputDimension,
+            outFeatures = _outputDimension,
             id = id,
             myInitWeights = weightsValue,
             myInitBias = biasValue
@@ -141,12 +151,18 @@ class DenseImpl(
             _activation = value
         }
 
+    override var units: Int
+        get() = _outputDimension
+        set(value) {
+            _outputDimension = value
+        }
+
     override fun weights(initBlock: (Shape) -> Tensor) {
-        weightsValue = initBlock(Shape(outputDimension, inputDimension))
+        weightsValue = initBlock(Shape(_outputDimension, inputDimension))
     }
 
     override fun bias(initBlock: (Shape) -> Tensor) {
-        biasValue = initBlock(Shape(outputDimension))
+        biasValue = initBlock(Shape(_outputDimension))
     }
 }
 
@@ -180,12 +196,14 @@ class MaxPool2dImpl(
     )
 }
 
-private class NeuralNetworkDslImpl : NeuralNetworkDsl {
-
+// Stage implementation
+class StageImpl(private val id: String) : NeuralNetworkDsl {
     val modules = mutableListOf<Module>()
     var lastDimension = 0
+    var inputDimension = 0
 
-    fun create() = NetworkBuilder().add(*modules.toTypedArray()).build()
+    fun create(): Module = MLP(*modules.toTypedArray(), name = id)
+
     override fun input(inputSize: Int, id: String) {
         lastDimension = inputSize
         modules.add(Input(Shape(inputSize), name = getDefaultName(id, "Input", modules.size)))
@@ -222,16 +240,132 @@ private class NeuralNetworkDslImpl : NeuralNetworkDsl {
         lastDimension = outputDimension
         val impl = DenseImpl(
             inputDimension = inputDimension,
-            outputDimension = outputDimension,
+            _outputDimension = outputDimension,
             id = getDefaultName(id, "linear", modules.size)
         )
         impl.content()
-        // dense layer consinst from linear module and activation function module (2 modules)
+        // dense layer consists of linear module and activation function module (2 modules)
+        modules += impl.create()
+    }
+
+    override fun dense(id: String, content: DENSE.() -> Unit) {
+        // This version of dense requires units to be specified in the content block
+        val impl = DenseImpl(
+            inputDimension = lastDimension,
+            _outputDimension = 0, // Will be set in content block via units property
+            id = getDefaultName(id, "linear", modules.size)
+        )
+        impl.content()
+        // Update lastDimension based on the units set in the content block
+        lastDimension = impl.outputDimension
+        // dense layer consists of linear module and activation function module (2 modules)
         modules += impl.create()
     }
 
     override fun activation(id: String, activation: (Tensor) -> Tensor) {
         modules += ActivationsWrapperModule(activation, getDefaultName(id, "activation", modules.size))
+    }
+
+    override fun sequential(content: NeuralNetworkDsl.() -> Unit) {
+        val sequentialImpl = NeuralNetworkDslImpl()
+        sequentialImpl.lastDimension = lastDimension
+        sequentialImpl.content()
+        lastDimension = sequentialImpl.lastDimension
+        modules += sequentialImpl.create()
+    }
+
+    override fun stage(id: String, content: NeuralNetworkDsl.() -> Unit) {
+        val stageImpl = StageImpl(id)
+        stageImpl.lastDimension = lastDimension
+        stageImpl.content()
+        lastDimension = stageImpl.lastDimension
+        modules += stageImpl.create()
+    }
+}
+
+private class NeuralNetworkDslImpl : NeuralNetworkDsl {
+
+    val modules = mutableListOf<Module>()
+    var lastDimension = 0
+
+    fun create() = NetworkBuilder().add(*modules.toTypedArray()).build()
+
+    override fun input(inputSize: Int, id: String) {
+        lastDimension = inputSize
+        modules.add(Input(Shape(inputSize), name = getDefaultName(id, "Input", modules.size)))
+    }
+
+    override fun flatten(id: String, content: FLATTEN.() -> Unit) {
+        val impl = FlattenImpl(
+            id = getDefaultName(id, "flatten", modules.size)
+        )
+        impl.content()
+        modules += impl.create()
+    }
+
+    override fun conv2d(id: String, content: CONV2D.() -> Unit) {
+        val impl = Conv2dImpl(
+            inChannels = lastDimension,
+            id = getDefaultName(id, "conv2d", modules.size)
+        )
+        impl.content()
+        lastDimension = impl.outChannels
+        modules += impl.create()
+    }
+
+    override fun maxPool2d(id: String, content: MAXPOOL2D.() -> Unit) {
+        val impl = MaxPool2dImpl(
+            id = getDefaultName(id, "maxPool2d", modules.size)
+        )
+        impl.content()
+        modules += impl.create()
+    }
+
+    override fun dense(outputDimension: Int, id: String, content: DENSE.() -> Unit) {
+        val inputDimension = lastDimension
+        lastDimension = outputDimension
+        val impl = DenseImpl(
+            inputDimension = inputDimension,
+            _outputDimension = outputDimension,
+            id = getDefaultName(id, "linear", modules.size)
+        )
+        impl.content()
+        // dense layer consists of linear module and activation function module (2 modules)
+        modules += impl.create()
+    }
+
+    override fun dense(id: String, content: DENSE.() -> Unit) {
+        // This version of dense requires units to be specified in the content block
+        val impl = DenseImpl(
+            inputDimension = lastDimension,
+            _outputDimension = 0, // Will be set in content block via units property
+            id = getDefaultName(id, "linear", modules.size)
+        )
+        impl.content()
+        // Update lastDimension based on the units set in the content block
+        lastDimension = impl.outputDimension
+        // dense layer consists of linear module and activation function module (2 modules)
+        modules += impl.create()
+    }
+
+    override fun activation(id: String, activation: (Tensor) -> Tensor) {
+        modules += ActivationsWrapperModule(activation, getDefaultName(id, "activation", modules.size))
+    }
+
+    override fun sequential(content: NeuralNetworkDsl.() -> Unit) {
+        val sequentialImpl = NeuralNetworkDslImpl()
+        sequentialImpl.lastDimension = lastDimension
+        sequentialImpl.content()
+        lastDimension = sequentialImpl.lastDimension
+        modules += sequentialImpl.create()
+    }
+
+    override fun stage(id: String, content: NeuralNetworkDsl.() -> Unit) {
+        val stageImpl = StageImpl(id)
+        stageImpl.lastDimension = lastDimension
+        stageImpl.content()
+        lastDimension = stageImpl.lastDimension
+        modules += stageImpl.create()
     }
 }
 
